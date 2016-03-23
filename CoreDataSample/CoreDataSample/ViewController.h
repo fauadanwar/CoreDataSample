b95f4d440e267ed8835845d0d31a81f391a1d012
FauadAnwar <fauad.anwar@M-3006.local>
2016-03-23 20:28:49 +0530

    Adde sample project

---
+++ CoreDataSample/CoreDataSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataSample
+//
+//  Created by FauadAnwar on 23/03/16.
+//  Copyright © 2016 Freelancer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataSample/CoreDataSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreDataSample
+//
+//  Created by FauadAnwar on 23/03/16.
+//  Copyright © 2016 Freelancer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
